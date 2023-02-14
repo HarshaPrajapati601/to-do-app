3cd1b2197b3c0bf605346193d030d5abebdf6c46
Harsha <harsha@sb-tech-106.local>
2023-02-14 22:55:46 +0530

    Debounce search api

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,37 +1,55 @@
-import React, { useContext } from "react";
-import { ToDoListContext } from "../context";
+import React, { useState, useEffect, useCallback } from "react";
 import Select from "react-select";
 
 const SearchBar = (props) => {
-  const context = useContext(ToDoListContext);
-  // const [selectedOptions, setSelectedOptions] = useState();
+  const [search, setSearch] = useState([]);
+
+
+  const debounce = (fn) => {
+    let timer;
+    return function(...args) {
+        const context = this;
+      if(timer) {
+        clearTimeout(timer);
+      }
+      timer = setTimeout(() => {
+        timer =null;
+        fn.apply(context, args);
+      }, 1000);
+    }
+  }
+  
+
+  const handleInput = (e) => {
+    const val = e.target.value
+    fetch(`https://demo.dataverse.org/api/search?q=${val}`)
+      .then((res) => res.json())
+      .then((json) => setSearch(json.data.items));
+  };
+
+  // useCb to memoize the debounce method and not call it on evry render
+  const optimisedVersion = useCallback(debounce(handleInput), []);
+
+  // const betterFunction = doSomeMagic(getData, 300);
+
   return (
     <div>
-      <h2>Choose your color</h2>
-      <div className="dropdown-container">
-        {/* <input id="input1" hidden="hidden" placeholder="input data" /> */}
-        {/* <select 
-        id="select1"
-        onChange={(e) => context.handleSelect(e.target.value)}
-        value={context.selectedOptions}
-        style={{width: 200, height: 50, padding: 10}} 
-         placeholder="Select color">
-            <option value="0">-make your own choice-</option>
-           return {context.optionsList.map(eachData => {
-            return <option key={eachData.value} value={eachData.value}>{eachData.label}</option>
-           })}
-        </select> */}
-        <Select
-          onChange={(e) => context.handleSelect(e.target.value)}
-          value={context.selectedOptions}
-          options={context.optionsList}
-        />
-        isSearchable={true}
-        placeholder="Select color"
-        isMulti
-      </div>
+      <h2>Debounce</h2>
       <div>
-        <h4></h4>
+        <input
+          type="text"
+          name="search"
+          onChange={optimisedVersion}
+          placeholder="Enter search..."
+          className="search"
+        />
+        {search?.length > 0 && (
+          <div className="autocomplete">
+            {search.map((ele, i) => {
+             return <ul key={i}><li>{ele.name}</li></ul>;
+            })}
+          </div>
+        )}
       </div>
     </div>
   );
